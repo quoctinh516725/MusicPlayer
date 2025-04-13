cde327ed3c1178b49173d950f7eaff66de34e31f
Ngo-Dinh-Anh-Tuan <tuannda3244@ut.edu.vn>
2025-04-13 12:25:00 +0700

    dfuhbjsn

--- api.js
+++ api.js
@@ -4,38 +4,7 @@ const songs = [
     singer: "Sơn Tùng MTP",
     thumb: "./img/dunglamtraitimanhdau.jpg",
     url: "./music/dunglamtraitimanhdau.mp3",
-  },
-  {
-    song: "Nổi gió rồi",
-    singer: "Châu Thâm",
-    thumb: "./img/noigioroi.jpg",
-    url: "./music/noigioroi.mp3",
-  },
-  {
-    song: "Mây",
-    singer: "Jank",
-    thumb: "./img/may.jpg",
-    url: "./music/may.mp3",
-  },
-
-  {
-    song: "Gió",
-    singer: "Jank",
-    thumb: "./img/gio.jpg",
-    url: "./music/gio.mp3",
-  },
-  {
-    song: "Summer Time",
-    singer: "SBC",
-    thumb: "./img/summerTime.jpg",
-    url: "./music/summertime.mp3",
-  },
-  {
-    song: "Lưu luyến sau chia tay",
-    singer: "Try92-Kay97",
-    thumb: "./img/luuluyensauchiatay.jpg",
-    url: "./music/luuluyensauchiatay.mp3",
-  },
+  }
 ];
 
 export { songs };
